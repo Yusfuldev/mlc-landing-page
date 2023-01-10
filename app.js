a15c32899cbd0e51976ea7ccbcea2f81ea1cf090
Yusfuldev <99514391+Yusfuldev@users.noreply.github.com>
2023-01-10 23:23:21 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const openMenu = document.querySelector(".burger");
+const closeMenu = document.querySelector(".close");
+const menu = document.querySelector("menu");
+const cardsContainer = document.querySelector(".cards");
+const cards = document.querySelectorAll(".card");
+const first = document.querySelector("#grey");
+const next = document.querySelector("#next");
+const head = document.querySelector("header");
+
+/***** Open/close nav-menu *****/
+openMenu.addEventListener("click", () => {
+  menu.classList.toggle("open-menu");
+  document.body.classList.toggle("opaque");
+  head.classList.toggle("opaque");
+});
+closeMenu.addEventListener("click", () => {
+  menu.classList.toggle("open-menu");
+  document.body.classList.toggle("opaque");
+  head.classList.toggle("opaque");
+});
+
+/***** carousel *****/
+next.addEventListener("click", () => {
+  cards.forEach((slide) => {
+    slidesWidth = slide.clientWidth;
+    cardsContainer.scrollLeft += slidesWidth;
+  });
+});
+
+first.addEventListener("click", () => {
+  cards.forEach((slide) => {
+    slidesWidth = slide.clientWidth;
+    cardsContainer.scrollLeft -= slidesWidth;
+  });
+});
